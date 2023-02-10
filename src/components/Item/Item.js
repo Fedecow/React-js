f7102b087e5902966280d3405aa635995350039e
Fedecow <gomezfede4@gmail.com>
2023-02-10 18:21:06 -0300

    cambio 10/2

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,7 +1,7 @@
 import './Item.css'
 const Item = ({producto}) => {
   return (
-    <li key={producto.id} width="200" className='product'>
+    <li width="200" className='product'>
       <img alt={producto.title} src={producto.image} width="200"/>
      <h2> {producto.title}</h2>
      <h3>{producto.category}</h3>
--- src/pages/ItemListContainer/ItemListContainer.js
+++ src/pages/ItemListContainer/ItemListContainer.js
@@ -1,26 +1,41 @@
 import ItemCount from '../../components/ItemCount/ItemCount'
 import { useEffect, useState } from 'react'
 import ItemList from '../../components/ItemList/ItemList'
-const arreglo = [
-{name:'producto 1', id: 'sj321' },
-{name:'producto 2', id: 'sj322' },
-{name:'producto 3', id: 'sj323' },
-{name:'producto 4', id: 'sj324' },
-{name:'producto 5', id: 'sj325' },
-{name:'producto 6', id: 'sj326' },
+ const arreglo = [
+ {name:'producto 1', id: 'sj321' },
+ {name:'producto 2', id: 'sj322' },
+ {name:'producto 3', id: 'sj323' },
+ {name:'producto 4', id: 'sj324' },
+ {name:'producto 5', id: 'sj325' },
+ {name:'producto 6', id: 'sj326' },
   
-] 
+ ] 
 const ItemListContainer = ({greeting}) => {
-const [ products, setProducts] = useState([])
- 
-// const getProducts = new Promise ((resolve, reject) => {
-//    setTimeout(() => {
-//     resolve(arreglo)
-//    }, 2000)
-    
-//   })
+  const [ products, setProducts] = useState([])
+  const [singleProduct, setSingleProduct] = useState ({});
+  const [productoCreado, setProductoCreado] = useState({})
+  const idProducto = '8'
 
-const getProducts = fetch('https://fakestoreapi.com/products') 
+const getProducts = fetch('https://fakestoreapi.com/products',{
+  method: 'GET',
+  headers:{'content-type':'json'}, 
+}) ;
+const getProduct = fetch(`https://fakestoreapi.com/products/${idProducto}`, {
+  method: 'GET',
+})
+
+const addProduct = fetch('https://fakestoreapi.com/products', {
+  method: 'POST',
+  body: JSON.stringify(
+    {
+        title: 'test product',
+        price: 13.5,
+        description: 'lorem ipsum set',
+        image: 'https://i.pravatar.cc',
+        category: 'electronic'
+    }
+)
+})
 
 
 
@@ -34,12 +49,39 @@ const getProducts = fetch('https://fakestoreapi.com/products')
      })
     .catch(error => console.log(error))
   }, [])
+
+
+  useEffect ( () => {
+getProduct
+.then((resp) => {
+  return resp.json()
+})
+.then ((data) => {
+  setSingleProduct (data)
+  // console.log(data)
+})
+.catch((error) => {console.log(error)})
+  }, [])
+  
   
   return (
     <div>
       {greeting}
       <ItemCount />
       <ItemList productos={products}/>
+      <h1>
+        Este es un producto especifico
+      </h1>
+      
+{/* 
+      <div width="200" className='product'>
+      <img alt={singleProduct.title} src={singleProduct.image} width="200"/>
+     <h2> {singleProduct.title}</h2>
+     <h3>{singleProduct.category}</h3>
+     <h3>{singleProduct.description}</h3>
+
+    </div> */}
+      
 
 
 </div>
